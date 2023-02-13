f60508c81caaf8fbdf5c06d114e0a066bdaad52d
Даниил <mackarosha@mail.ru>
2023-02-13 18:35:03 +1000

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("**       **    **    *  ***  ***  *");
+        System.out.println("*  *    *  *   * *   *   *    *   *");
+        System.out.println("*   *  *    *  *  *  *   *    *   *");
+        System.out.println("*  *   ******  *   * *   *    *   *");
+        System.out.println("**     *    *  *    **  ***  ***  *****");
+        System.out.println("Спасибо за ваш выбор");
+        System.out.println(1+2+3+4+5+6+7+8+9+10);
+        System.out.println(1*2*3*4*5*6*7*8*9*10);
+    }
+}
